80ee8d09def71aca0601977ed43d48545c26a25e
Kysiam <how2logout@gmail.com>
2023-11-23 06:15:34 +0900

    style: make a button hover animation and container background darker

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const darkBox = document.getElementById(blind);
-const menuHover = document.querySelector('.menu-item');
+const bottomMenu = document.querySelector('.bottom__menu');
+const container = document.getElementById('container');
+const menuItems = document.querySelectorAll('.menu-item');
+const dropdowns = document.querySelectorAll('.dropdown');
 
-menuHover.addEventListener('mouseover', () => {});
+// bottomMenu.addEventListener('mouseover', (e) => {
+//   container.classList.add('on');
+// });
+
+// bottomMenu.addEventListener('mouseout', (e) => {
+//   container.classList.remove('on');
+// });
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('mouseover', (e) => {
+    console.log(e.currentTarget);
+    const isMenuItem = e.currentTarget.classList.contains('menu-item');
+    if (isMenuItem) {
+      container.classList.add('on');
+    }
+  });
+});
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('mouseout', (e) => {
+    container.classList.remove('on');
+  });
+});
